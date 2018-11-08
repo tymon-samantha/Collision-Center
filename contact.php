10c61a00127685413fe2fea779d8a7eb56c8cbaf
Samantha Tymon <tymon.samantha@gmail.com>
2018-11-08 14:27:47 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
 <?php 
-  $name = $_POST["name"]; 
-  $address = $_POST["address"]; 
-  $phone = $_POST["phone"]; 
-  $email = $_POST["email"]; 
-  $date = $_POST["date"]; 
-  $insurance = $_POST["insurance"]; 
-  $message = $_POST["message"]; 
-  $formcontent="From: $name \n Details: $messsage"; 
+  $name = $_POST['name']; 
+  $address = $_POST['address']; 
+  $phone = $_POST['phone']; 
+  $email = $_POST['email']; 
+  $date = $_POST['date']; 
+  $insurance = $_POST['insurance']; 
+  $message = $_POST['message']; 
+  $formcontent="From: $name \n Address: $address \n Phone: $phone \n Email: $email \n Date: $date \n Insurance: $insurance \n Details: $messsage"; 
   $recipient = "stymon@dealerappvantage.com";
   $subject = "Accident Reporting Form"; 
   $mailheader = "From: $email \r\n"; 
   mail ($recipient, $subject, $formcontent, $mailheader) or die ("Error!");
-  echo "Thank You! Your request has been sent.";
+  echo "Thank You! Your request has been sent." . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
