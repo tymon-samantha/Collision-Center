003c548841bbad101097ada98535e59b111e0379
Samantha Tymon <tymon.samantha@gmail.com>
2018-11-08 14:42:16 -0500

    add netlify form

--- contact.php
+++ contact.php
@@ -11,5 +11,5 @@
   $subject = "Accident Reporting Form"; 
   $mailheader = "From: $email \r\n"; 
   mail ($recipient, $subject, $formcontent, $mailheader) or die ("Error!");
-  echo "Thank You! Your request has been sent." . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+  echo "Thank You! Your request has been sent."
 ?>
